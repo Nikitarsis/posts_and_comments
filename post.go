3cac987ee666d47d935f6b0a24b88b725b810e68
Nikita <Nik-Mir-Vas@yandex.ru>
2025-01-30 17:03:26 +0300

    Changed post.go

--- post.go
+++ post.go
@@ -2,7 +2,7 @@ package comments_and_posts
 
 type Post struct {
 	postId      msgId
-	commentsIds []msgId
+	commentsIds map[msgId]struct{}
 }
 
 func (p Post) GetMessageId() msgId {
@@ -10,5 +10,35 @@ func (p Post) GetMessageId() msgId {
 }
 
 func (p *Post) SetMessageId(id msgId) {
-	m.messageId = id
+	p.postId = id
+}
+
+func (p Post) GetCommentsId() []msgId {
+	ret := make([]msgId, len(p.commentsIds))
+	i := 0
+	for k := range p.commentsIds {
+		ret[i] = k
+		i++
+	}
+	return ret
+}
+
+func (p *Post) AddCommentId(ids ...msgId) {
+	for _, id := range ids {
+		p.commentsIds[id] = struct{}{}
+	}
+}
+
+func (p *Post) RemoveCommentId(ids ...msgId) {
+	for _, id := range ids {
+		delete(p.commentsIds, id)
+	}
+}
+
+func NewPost(id msgId, commentIds ...msgId) *Post {
+	comMap := make(map[msgId]struct{}, len(commentIds))
+	for commentId := range commentIds {
+		comMap[msgId(commentId)] = struct{}{}
+	}
+	return &Post{id, comMap}
 }
