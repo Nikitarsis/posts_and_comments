bf281f0ac9c1feb942a3d4f112553dc60dd27862
Nikita <Nik-Mir-Vas@yandex.ru>
2025-01-31 01:00:15 +0300

    Add comments to message

--- message.go
+++ message.go
@@ -1,5 +1,6 @@
 package comments_and_posts
 
+// Тип сообщения
 type POST_TYPE string
 
 const (
@@ -7,23 +8,39 @@ const (
 	COMMENT POST_TYPE = "comment"
 )
 
+/*
+Сообщение внутри комментария, поста etc. Его содержательная часть
+Содержит уникальный идентифекатор и само содержание
+*/
 type Message struct {
 	messageId      msgId
 	contentMessage content
 }
 
+/*
+Возвращает ID сообщения
+*/
 func (m Message) GetMessageId() msgId {
 	return m.messageId
 }
 
+/*
+Получает содержание сообщения
+*/
 func (m Message) GetContent() content {
 	return m.contentMessage
 }
 
+/*
+Обновляет содержание сообщения
+*/
 func (m *Message) SetContent(data content) {
 	m.contentMessage = data
 }
 
+/*
+Простой конструктор
+*/
 func NewMessage(id msgId, data content) *Message {
 	return &Message{id, data}
 }
