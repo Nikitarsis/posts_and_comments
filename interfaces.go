ff050a095c8b3df4c29706da7c08059e33ca0d17
Nikita <Nik-Mir-Vas@yandex.ru>
2025-01-30 23:54:14 +0300

    Add commentaries

--- interfaces.go
+++ interfaces.go
@@ -1,18 +1,27 @@
 package comments_and_posts
 
-type usrId uint64
+/*
+Основные интерфейсы пакета
+*/
+
+// Обложка для ID
 type msgId uint64
+
+// Обложка для сообщения
 type content string
 
+// Структура имеет ID сообщения
 type IHaveMessageId interface {
 	GetMessageId() msgId
 }
 
+// Структура содержит содержание(текст, ссылки на прикреплёные файлы etc.)
 type IHaveContent interface {
 	GetContent() content
 	SetContent(data content)
 }
 
+// Структура поддерживает комментарии
 type IHaveComments interface {
 	GetCommentsId() []msgId
 	AddCommentsId(ids ...msgId)
@@ -20,27 +29,32 @@ type IHaveComments interface {
 	GetCommentsNum() uint
 }
 
+// Структура может иметь родительский элемент(для комментариев-ответов)
 type IHaveParent interface {
 	GetParentId() msgId
 	SetParentId(id msgId)
 }
 
+// Структура может иметь дочерние элементы(для комментариев с ответами)
 type IHaveChildren interface {
 	GetChildrenIds() []msgId
 	AddChildrenIds(ids ...msgId)
 	RemoveChildrenIds(ids ...msgId)
 }
 
+// Сообщение с Id и содержанием
 type IMessage interface {
 	IHaveMessageId
 	IHaveContent
 }
 
+// Пост, который может иметь комментарии
 type IPost interface {
 	IHaveMessageId
 	IHaveComments
 }
 
+// Комментарий, который может ссылаться
 type IComment interface {
 	IHaveMessageId
 	IHaveParent
