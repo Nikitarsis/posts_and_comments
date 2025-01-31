3f606b081bd2d4dafe788ef937e21f23bfdbd064
Nikita <Nik-Mir-Vas@yandex.ru>
2025-01-31 13:34:49 +0300

    Add comments to interfaces.go

--- interfaces.go
+++ interfaces.go
@@ -12,23 +12,29 @@ type content string
 
 // Структура имеет ID сообщения
 type IHaveMessageId interface {
+	//Возвращает свой ID
 	GetMessageId() msgId
 }
 
 // Структура содержит содержание(текст, ссылки на прикреплёные файлы etc.)
 type IHaveContent interface {
+	//Возвращает содержание
 	GetContent() content
+	//Задаёт содержание
 	SetContent(data content)
 }
 
 // Структура может иметь родительский элемент(для комментариев-ответов)
 type IHaveParent interface {
+	//Возвращает ID родителя и false, либо другой ID(напр. собственный) и true, если родителя нет
 	GetParentId() (msgId, bool)
 }
 
 // Структура может иметь дочерние элементы(для комментариев с ответами)
 type IHaveChildren interface {
+	//Возвращает ID дочерних собщений
 	GetChildrenIds() []msgId
+	//Добавляет дочерние сообщения
 	AddChildrenIds(ids ...msgId)
 }
 
