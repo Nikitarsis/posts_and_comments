f3565c833d701bb9b346443e53dfeb5263bf4f85
Nikita <Nik-Mir-Vas@yandex.ru>
2025-02-01 12:56:14 +0300

    Changed server

--- server.go
+++ server.go
@@ -1,9 +1,20 @@
 package graphql
 
 import (
-	eggql "github.com/andrewwphillips/eggql"
+	"net/http"
+
+	"github.com/andrewwphillips/eggql"
 )
 
-func aaa() {
-	eggql.NilResolver(false)
+func checkPosts(w http.ResponseWriter, r *http.Request) {
+	eggql.InitialTimeout(1)
+}
+
+func StartServer() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World!"))
+	})
+	mux.HandleFunc("posts", checkPosts)
+
 }
