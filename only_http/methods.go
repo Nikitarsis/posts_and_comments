3fd48e52a7622399fda53f91fb769bb93d74c21a
Nikita <Nik-Mir-Vas@yandex.ru>
2025-02-02 16:30:16 +0300

    Finished http methods

--- only_http/methods.go
+++ only_http/methods.go
@@ -22,10 +22,10 @@ type ServerCallbacks struct {
 	deletePost    func(post uint64, user uint64) tdao.PROBLEM
 	mutePost      func(post uint64, user uint64) tdao.PROBLEM
 	unmutePost    func(post uint64, user uint64) tdao.PROBLEM
-	getComment    func(comment uint64) tdao.PROBLEM
+	getComment    func(comment uint64) (tdao.PROBLEM, *tdao.CommentDao)
 	postComment   func(user uint64, message *string) (uint64, tdao.PROBLEM)
 	updateComment func(comment uint64, user uint64, message *string) tdao.PROBLEM
-	deleteComment func(comment uint64, user uint64)
+	deleteComment func(comment uint64, user uint64) tdao.PROBLEM
 }
 
 func (s ServerCallbacks) Test(w http.ResponseWriter, r *http.Request) {
@@ -78,7 +78,7 @@ func (s ServerCallbacks) post_get(w http.ResponseWriter, postStr string, from st
 
 func (s ServerCallbacks) post_post(w http.ResponseWriter, reader io.ReadCloser, post string, user string) {
 	bytes := make([]byte, 0)
-	createNew := post == "" 
+	createNew := post == ""
 	userId, errUser := strconv.ParseUint(user, 16, 64)
 	if errUser != nil {
 		w.WriteHeader(http.StatusBadRequest)
@@ -93,19 +93,41 @@ func (s ServerCallbacks) post_post(w http.ResponseWriter, reader io.ReadCloser, 
 	}
 	str := string(bytes)
 	if createNew {
-		retId, response := s.postPost(userId, &str)
+		retId, _ := s.postPost(userId, &str)
 		id_str := strconv.FormatUint(retId, 16)
 		w.Write([]byte(fmt.Sprint("\"post_id\":\"%s\"", id_str)))
 		return
 	}
-	postId, errPost := strconv.ParseUint(postStr, 16, 64)
-	if errUser != nil {
+	postId, errPost := strconv.ParseUint(post, 16, 64)
+	if errPost != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte("unable to parse from hex post_id"))
 		return
 	}
-	problem := s.updatePost(postId, userId, bytes)
+	problem := s.updatePost(postId, userId, &str)
 	switch problem {
+	case tdao.INCORRECT_USER:
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte("Incorrect user"))
+	}
+	w.WriteHeader(http.StatusAccepted)
+}
+
+func (s ServerCallbacks) post_delete(w http.ResponseWriter, user string, post string) {
+	userId, errUser := strconv.ParseUint(user, 16, 64)
+	if errUser != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("unable to parse from hex user_id"))
+		return
+	}
+	postId, errPost := strconv.ParseUint(post, 16, 64)
+	if errPost != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("unable to parse from hex post_id"))
+		return
+	}
+	ret := s.deletePost(postId, userId)
+	switch ret {
 	case tdao.INCORRECT_USER:
 		w.WriteHeader(http.StatusUnauthorized)
 		w.Write([]byte("Incorrect user"))
@@ -126,7 +148,120 @@ func (s ServerCallbacks) Post(w http.ResponseWriter, r *http.Request) {
 		user := header.Get("user_id")
 		message := r.Body
 		s.post_post(w, message, post, user)
-	case http.Method
+	case http.MethodDelete:
+		user := header.Get("user_id")
+		s.post_delete(w, user, post)
+	}
+}
+
+func (s ServerCallbacks) comment_get(w http.ResponseWriter, comment string) {
+	commentId, errCom := strconv.ParseUint(comment, 16, 64)
+	if errCom != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("unable to parse from hex comment_id"))
+		return
+	}
+	problem, result := s.getComment(commentId)
+	switch problem {
+	case tdao.NO_SUCH_POST:
+		w.WriteHeader(http.StatusNotFound)
+		return
+	case tdao.NO_PROBLEM:
+		message, err := json.Marshal(result)
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+		w.WriteHeader(http.StatusAccepted)
+		w.Write(message)
+		return
+	}
+}
+
+func (s ServerCallbacks) comment_post(w http.ResponseWriter, reader io.ReadCloser, comment string, user string) {
+	bytes := make([]byte, 0)
+	createNew := comment == ""
+	userId, errUser := strconv.ParseUint(user, 16, 64)
+	if errUser != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("unable to parse from hex user_id"))
+		return
+	}
+	_, err := reader.Read(bytes)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("cannot read body"))
+		return
+	}
+	str := string(bytes)
+	if createNew {
+		retId, _ := s.postPost(userId, &str)
+		id_str := strconv.FormatUint(retId, 16)
+		w.Write([]byte(fmt.Sprint("\"post_id\":\"%s\"", id_str)))
+		return
+	}
+	commentId, errCom := strconv.ParseUint(comment, 16, 64)
+	if errCom != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("unable to parse from hex post_id"))
+		return
+	}
+	problem := s.updatePost(commentId, userId, &str)
+	switch problem {
+	case tdao.INCORRECT_USER:
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte("Incorrect user"))
+		return
+	case tdao.NO_SUCH_POST:
+		id_str := strconv.FormatUint(commentId, 16)
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte(fmt.Sprint("Post with id %s not found", id_str)))
+		return
+	}
+	w.WriteHeader(http.StatusAccepted)
+}
+
+func (s ServerCallbacks) comment_delete(w http.ResponseWriter, user string, comment string) {
+	userId, errUser := strconv.ParseUint(user, 16, 64)
+	if errUser != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("unable to parse from hex user_id"))
+		return
+	}
+	commentId, errCom := strconv.ParseUint(comment, 16, 64)
+	if errCom != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("unable to parse from hex post_id"))
+		return
+	}
+	problem := s.deleteComment(commentId, userId)
+	switch problem {
+	case tdao.NO_SUCH_POST:
+		id_str := strconv.FormatUint(commentId, 16)
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte(fmt.Sprint("Comment with id %s doesn't exist", id_str)))
+		return
+	case tdao.INCORRECT_USER:
+		w.WriteHeader(http.StatusNotAcceptable)
+		w.Write([]byte("Incorrect User"))
+	}
+	w.WriteHeader(http.StatusAccepted)
+}
+
+func (s ServerCallbacks) Comment(w http.ResponseWriter, r *http.Request) {
+	method := r.Method
+	header := r.Header
+	comment := header.Get("comment_id")
+	switch method {
+	case http.MethodGet:
+		s.comment_get(w, comment)
+	case http.MethodPost:
+		user := header.Get("user_id")
+		message := r.Body
+		s.comment_post(w, message, comment, user)
+	case http.MethodDelete:
+		user := header.Get("user_id")
+		s.comment_delete(w, user, comment)
 	}
 }
 
