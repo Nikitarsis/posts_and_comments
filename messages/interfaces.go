f52147500348e4f6b35bfbc4988352a370519038
Nikita <Nik-Mir-Vas@yandex.ru>
2025-02-03 17:45:39 +0300

    Add methods to messages

--- messages/interfaces.go
+++ messages/interfaces.go
@@ -1,21 +1,5 @@
 package messages
 
-type MsgId struct {
-	uint64
-}
-
-func (m MsgId) GetId() uint64 {
-	return m.uint64
-}
-
-func (m MsgId) IsLess(c IMsgId) bool {
-	return m.GetId() < c.GetId()
-}
-
-func GetMessageId(msg uint64) MsgId {
-	return MsgId{msg}
-}
-
 type IMsgId interface {
 	GetId() uint64
 	IsLess(c IMsgId) bool
--- messages/message_id.go
+++ messages/message_id.go
@@ -1,5 +1,11 @@
 package messages
 
+import "time"
+
+var hasTimer bool
+var lastTime uint64
+var counter uint16
+
 type MsgId struct {
 	uint64
 }
@@ -14,4 +20,24 @@ func (m MsgId) IsLess(c IMsgId) bool {
 
 func GetMessageId(msg uint64) MsgId {
 	return MsgId{msg}
+}
+
+func GetNewMessageId() MsgId {
+	sec := uint64(time.Now().UnixMilli())
+	ret := sec << 22
+	var max uint64 = 1 << 63
+	ret = ret &^ max
+	ret = ret | uint64(counter)
+	if !hasTimer {
+		lastTime = sec
+		counter = 0
+		return MsgId{ret}
+	}
+	if lastTime != sec {
+		lastTime = sec
+		counter = 0
+		return MsgId{ret}
+	}
+	counter++
+	return MsgId{ret}
 }
