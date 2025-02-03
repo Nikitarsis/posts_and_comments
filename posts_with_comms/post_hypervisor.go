6c51a613fbde90ab1ec39d2fa298758d185bf31a
Nikita <Nik-Mir-Vas@yandex.ru>
2025-02-03 23:17:54 +0300

    Add methods

--- posts_with_comms/post_hypervisor.go
+++ posts_with_comms/post_hypervisor.go
@@ -5,7 +5,7 @@ import (
 )
 
 type PostHypervisor struct {
-	initPostList       map[messages.MsgId]CommentPost
+	initPostList       map[messages.MsgId]*CommentPost
 	associatedPostList map[messages.MsgId]messages.MsgId
 }
 
@@ -24,13 +24,49 @@ func (p PostHypervisor) GetPost(id messages.MsgId) (*CommentPost, bool) {
 	if !check {
 		return nil, false
 	}
-	return &ret, true
+	return ret, true
+}
+
+func (p PostHypervisor) GetComment(id messages.MsgId) (IPost, bool) {
+	parent := p.associatedPostList[id]
+	if parent == id {
+		return nil, false
+	}
+	comPost, check := p.initPostList[parent]
+	if !check {
+		return nil, false
+	}
+	ret, err := comPost.GetComments(id)
+	if err != nil {
+		return nil, false
+	}
+	return ret[0], true
 }
 
 func (p *PostHypervisor) NewPost(post messages.MsgId) {
 	comPost := NewCommentPost(post)
-	p.initPostList[post] = *comPost
+	p.initPostList[post] = comPost
 	p.associatedPostList[post] = post
+}
+
+func (p *PostHypervisor) NewComment(parent messages.MsgId, comment messages.MsgId) {
+	post := p.associatedPostList[parent]
+	p.initPostList[post].AddCommentsToPost(comment)
+	p.associatedPostList[comment] = post
+}
+
+func (p *PostHypervisor) DeletePost(post messages.MsgId) {
+	_, check := p.initPostList[post]
+	if !check {
+		return
+	}
+	delete(p.initPostList, post)
+	for key, value := range p.associatedPostList {
+		if value == post {
+			delete(p.associatedPostList, key)
+		}
+	}
+	delete(p.associatedPostList, post)
 }
 
 func (p PostHypervisor) HasPost(post messages.MsgId) bool {
@@ -40,7 +76,7 @@ func (p PostHypervisor) HasPost(post messages.MsgId) bool {
 
 func NewPostHypervisor() PostHypervisor {
 	return PostHypervisor{
-		initPostList:       make(map[messages.MsgId]CommentPost),
+		initPostList:       make(map[messages.MsgId]*CommentPost),
 		associatedPostList: make(map[messages.MsgId]messages.MsgId),
 	}
 }
